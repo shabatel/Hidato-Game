835b50424217e23ccb3ebfbaa932cf977585e36a
shabatel <elad155@gmail.com>
2021-01-19 21:12:55 +0200

    Graphical user interface added

---
+++ game.py
@@ -0,0 +1,225 @@
+import time
+from solver import valid
+import pygame
+
+# Constants
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+# TODO: add grey
+pygame.font.init()
+
+
+class Grid:
+    board = [
+        [9, 0, 0],
+        [0, 4, 1],
+        [7, 0, 5]
+    ]
+
+    def __init__(self, rows, cols, width, height):
+        self.rows = rows
+        self.cols = cols
+        self.width = width
+        self.height = height
+        self.cubes = [[Cube(self.board[i][j], i, j, width, height) for j in range(cols)] for i in range(rows)]
+        self.selected = None
+        self.model = None
+
+    def draw(self, screen):
+        # Draw Grid Lines
+        gap = self.width / 3
+        for i in range(self.rows + 1):
+            thick = 1
+            pygame.draw.line(screen, (0, 0, 0), (0, i * gap), (self.width, i * gap), thick)
+            pygame.draw.line(screen, (0, 0, 0), (i * gap, 0), (i * gap, self.height), thick)
+
+        # Draw Cubes
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.cubes[i][j].draw(screen)
+
+    def place(self, val):
+        row, col = self.selected
+        if self.cubes[row][col].value == 0:
+            self.cubes[row][col].set(val)
+            self.update_model()
+
+            if valid(self.model, val, (row, col)):
+                return True
+            else:
+                self.cubes[row][col].set_tmp(0)
+                self.cubes[row][col].set(0)
+                self.update_model()
+                return False
+
+    def clear(self):
+        row, col = self.selected
+        # check that the cube is not set value already
+        if self.cubes[row][col].value == 0:
+            self.cubes[row][col].set_tmp(0)
+
+    def is_finished(self):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if self.cubes[i][j].value == 0:
+                    return False
+        return True
+
+    def click(self, pos):
+        if pos[0] < self.width and pos[1] < self.height:
+            gap = self.width / 3
+            x = pos[0] // gap
+            y = pos[1] // gap
+            return (int(y), int(x))
+        else:
+            return None
+
+    def select(self, row, col):
+        # Reset all other
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.cubes[i][j].selected = False
+
+        self.cubes[row][col].selected = True
+        self.selected = (row, col)
+
+    def update_model(self):
+        self.model = [[self.cubes[i][j].value for j in range(self.cols)] for i in range(self.rows)]
+
+    def sketch(self, val):
+        row, col = self.selected
+        self.cubes[row][col].set_tmp(val)
+
+
+class Cube:
+    rows = 9
+    cols = 9
+
+    def __init__(self, value, row, col, width, height):
+        self.value = value
+        self.tmp = 0
+        self.row = row
+        self.col = col
+        self.width = width
+        self.height = height
+        self.selected = False
+
+    def draw(self, screen):
+        fnt = pygame.font.SysFont("comicsans", 100)
+
+        gap = self.width / 3
+        x = self.col * gap
+        y = self.row * gap
+
+        if self.tmp != 0 and self.value == 0:
+            text = fnt.render(str(self.tmp), 1, (128, 128, 128))
+            screen.blit(text, (x + 5, y + 5))
+        elif not (self.value == 0):
+            text = fnt.render(str(self.value), 1, (0, 0, 0))
+            screen.blit(text, (x + (gap / 2 - text.get_width() / 2), y + (gap / 2 - text.get_height() / 2)))
+
+        if self.selected:
+            pygame.draw.rect(screen, (255, 0, 0), (x, y, gap, gap), 3)
+
+    def set(self, val):
+        self.value = val
+
+    def set_tmp(self, val):
+        self.tmp = val
+
+
+def redraw_screen(screen, board, time1, strikes):
+    screen.fill(WHITE)
+    # Draw time
+    font = pygame.font.SysFont("comicsans", 40)
+    text = font.render("Time: " + format_time(time1), 1, BLACK)
+    screen.blit(text, (540 - 160, 560))
+    # Draw strikers
+    text = font.render("X " * strikes, 1, RED)
+    screen.blit(text, (20, 560))
+    # Draw Grid and board
+    board.draw(screen)
+
+
+def format_time(secs):
+    sec = secs % 60
+    minute = secs // 60
+
+    mat = " " + str(minute) + ":" + str(sec)
+    return mat
+
+
+def main():
+    screen = pygame.display.set_mode((540, 600))
+    pygame.display.set_caption("Hidato")
+    board = Grid(3, 3, 540, 540)
+    run = True
+    key = None
+    strikes = 0
+    start = time.time()
+    while run:
+        play_time = round(time.time() - start)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            # User using keyboard
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    key = 1
+                if event.key == pygame.K_2:
+                    key = 2
+                if event.key == pygame.K_3:
+                    key = 3
+                if event.key == pygame.K_4:
+                    key = 4
+                if event.key == pygame.K_5:
+                    key = 5
+                if event.key == pygame.K_6:
+                    key = 6
+                if event.key == pygame.K_7:
+                    key = 7
+                if event.key == pygame.K_8:
+                    key = 8
+                if event.key == pygame.K_9:
+                    key = 9
+                if event.key == pygame.K_r:
+                    # TODO: add 'press r for restart'
+                        pygame.quit()
+                        pygame.font.init()
+                        main()
+                if event.key == pygame.K_DELETE:
+                    board.clear()
+                    key = None
+                if event.key == pygame.K_RETURN:
+                    # Try placing the User typing
+                    row, col = board.selected
+                    if board.cubes[row][col].tmp != 0:
+                        if board.place(board.cubes[row][col].tmp):
+                            print("Success")
+                            if board.is_finished():
+                                print("Well Done")
+                                run = False
+                        else:
+                            print("Wrong")
+                            strikes += 1
+                        key = None
+
+            # User using mouse
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                clicked = board.click(pos)
+                if clicked:
+                    board.select(clicked[0], clicked[1])
+                    key = None
+
+        if board.selected and key is not None:
+            board.sketch(key)
+
+        redraw_screen(screen, board, play_time, strikes)
+        pygame.display.update()
+
+
+main()
+pygame.quit()
--- solver.py
+++ solver.py
@@ -1,59 +1,38 @@
-import datetime
+# import datetime
 
-# 9 X 9 MEDIUM
-board = [
-    [60,59,1,0,6,25,0,18,0],
-    [61,58,0,7,4,23,26,16,0],
-    [57,62,0,0,0,27,0,20,15],
-    [56,54,0,0,28,0,0,11,14],
-    [55,0,80,50,30,0,10,0,12],
-    [0,0,0,81,0,0,0,0,0],
-    [78,0,67,0,0,0,37,33,35],
-    [75,72,69,0,48,0,0,0,42],
-    [74,0,71,70,0,47,44,40,41]
-]
-
-numbers_in_used = [54,1,0,0,1,0,1,1,0,0,
-                     1,1,1,0,1,1,1,0,1,
-                     0,1,0,0,1,0,1,1,1,
-                     1,0,1,0,0,1,0,1,0,
-                     1,0,0,1,1,1,0,1,0,
-                     0,1,1,0,1,0,0,0,1,
-                     1,1,1,1,1,1,1,1,0,
-                     0,0,0,1,0,1,1,1,1,
-                     0,1,1,0,0,1,0,1,1]
+numbers_in_used = [5, 1, 0, 0, 1, 1, 0, 1, 0, 1]
 
 
-def solve(bo):
-    find = find_empty(bo)
+def solve(board):
+    find = find_empty(board)
     if not find:
         return True
     else:
         row, col = find
 
     for i in range(2, len(board) ** 2):
-        if valid(bo, i, (row, col)):
-            bo[row][col] = i
+        if valid(board, i, (row, col)):
+            board[row][col] = i
             numbers_in_used[i] = 1
             numbers_in_used[0] += 1
 
-            if solve(bo):
+            if solve(board):
                 return True
 
-            bo[row][col] = 0
+            board[row][col] = 0
             numbers_in_used[i] = 0
             numbers_in_used[0] -= 1
 
     return False
 
 
-def valid(bo, num, pos):
+def valid(board, num, pos):
     if numbers_in_used[num] == 1:
         return False
 
-    num_of_cells = get_num_of_empty_cells(pos[0], pos[1], num)
-    if check_if_neighbor_exist(pos[0], pos[1], num - 1):
-        if check_if_neighbor_exist(pos[0], pos[1], num + 1):
+    num_of_cells = get_num_of_empty_cells(board, pos[0], pos[1], num)
+    if check_if_neighbor_exist(board, pos[0], pos[1], num - 1):
+        if check_if_neighbor_exist(board, pos[0], pos[1], num + 1):
             return True
         else:
             if num_of_cells > 0 and numbers_in_used[num + 1] == 0:
@@ -63,7 +42,7 @@ def valid(bo, num, pos):
     else:
         if num_of_cells > 0 and numbers_in_used[num - 1] == 0:
             num_of_cells -= 1
-            if check_if_neighbor_exist(pos[0], pos[1], num + 1):
+            if check_if_neighbor_exist(board, pos[0], pos[1], num + 1):
                 return True
             elif num_of_cells > 0 and numbers_in_used[num + 1] == 0:
                 return True
@@ -73,7 +52,7 @@ def valid(bo, num, pos):
             return False
 
 
-def check_if_neighbor_exist(x, y, n):
+def check_if_neighbor_exist(board, x, y, n):
     for i in range(x - 1, x + 2):
         if 0 <= i < len(board):
             for j in range(y - 1, y + 2):
@@ -84,7 +63,7 @@ def check_if_neighbor_exist(x, y, n):
     return False
 
 
-def get_num_of_empty_cells(x, y, n):
+def get_num_of_empty_cells(board, x, y, n):
     empty_cells = 0
     for i in range(x - 1, x + 2):
         if 0 <= i < len(board):
@@ -99,17 +78,17 @@ def get_num_of_empty_cells(x, y, n):
 def print_board(bo):
     for i in range(len(bo)):
         for j in range(len(bo[0])):
-            if j % len(board) == 0 and j != 0:
+            if j % len(bo) == 0 and j != 0:
                 print(" | ", end="")
 
-            if j == len(board) - 1:
+            if j == len(bo) - 1:
                 print(bo[i][j])
             else:
                 print(str(bo[i][j]) + " ", end="")
 
 
 def find_empty(bo):
-    if numbers_in_used[0] != len(board)**2:
+    if numbers_in_used[0] != len(bo) ** 2:
         for i in range(len(bo)):
             for j in range(len(bo[0])):
                 if bo[i][j] == 0:
@@ -117,12 +96,11 @@ def find_empty(bo):
 
     return None
 
-
-print(len(board) ** 2)
-print_board(board)
-start_time = datetime.datetime.now()
-solve(board)
-time_delta = datetime.datetime.now() - start_time
-print("--------")
-print_board(board)
-print(time_delta)
+# print(len(board) ** 2)
+# print_board(board)
+# start_time = datetime.datetime.now()
+# solve(board)
+# time_delta = datetime.datetime.now() - start_time
+# print("--------")
+# print_board(board)
+# print(time_delta)
